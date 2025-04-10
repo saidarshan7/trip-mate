5ee858fe3a0ee373329c269dc515908eecd8f64e
saidarshan74 <141834083+saidarshan7@users.noreply.github.com>
2025-04-10 18:24:34 +0530

    added index.js

--- BE/index.js
+++ BE/index.js
@@ -1,29 +1,96 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const handlebars = require('handlebars');
-const dataset = require('./data');
+const express = require('express')
+const cors =require('cors')
+const app = express()
+const bodyParser = require('body-parser')
+const mainRoutes = require('./Routes/route')
+const PORT = 3000
 
-// console.log(data)
+app.use(cors())
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true // if you're using cookies or authentication headers
+// }));
+app.use(bodyParser.json());
+
+app.use('/discover',mainRoutes)
 
 
-(async () => {
-   const data = await dataset.dataset()
-  const templateHtml = fs.readFileSync('./templates/itinerary.hbs', 'utf8');
-  const template = handlebars.compile(templateHtml);
-  const html = template(data);
 
 
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setContent(html, { waitUntil: 'networkidle0' });
 
-  await page.pdf({
-    path: '5_Days_Trip_Plan_In_.pdf',
-    format: 'A4',
-    printBackground: true,
-  });
 
-  await browser.close();
-  console.log("✅ PDF Generated: itinerary.pdf");
-})();
+app.listen(PORT,()=>{
+  console.log(`listening on PORT ${PORT} `)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const puppeteer = require('puppeteer');
+// const fs = require('fs');
+// const handlebars = require('handlebars');
+// const dataset = require('./data');
+
+// // console.log(data)
+
+
+// (async () => {
+//    const data = await dataset.dataset()
+//   const templateHtml = fs.readFileSync('./templates/itinerary.hbs', 'utf8');
+//   const template = handlebars.compile(templateHtml);
+//   const html = template(data);
+
+
+//   const browser = await puppeteer.launch();
+//   const page = await browser.newPage();
+//   await page.setContent(html, { waitUntil: 'networkidle0' });
+
+//   await page.pdf({
+//     path: '5_Days_Trip_Plan_In_.pdf',
+//     format: 'A4',
+//     printBackground: true,
+//   });
+
+//   await browser.close();
+//   console.log("✅ PDF Generated: itinerary.pdf");
+// })();
 
